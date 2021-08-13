1b964324b9975cb5beb33b9b5a365b75ec3588b5
Dane Acena <acena.dane@gmail.com>
2021-08-12 21:03:00 -0700

    Added Xcode project files to github repository

---
+++ Gopher/Gopher/GopherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GopherApp.swift
+//  Gopher
+//
+//  Created by Dane Acena on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GopherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
